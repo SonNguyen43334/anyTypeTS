52befe3ea86ad16a86426140274ee461eaf4737f
son133 <Sonnguyen43334@gmail.com>
2023-11-28 15:44:53 +0700

    cm

---
+++ src/screens/WelcomScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const WelcomScreen = () => {
+    return (
+        <View>
+            <Text>WelcomScreen</Text>
+        </View>
+    )
+}
+
+export default WelcomScreen
+
+const styles = StyleSheet.create({})
