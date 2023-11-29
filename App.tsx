111066fd1de19a5e6b71777422326eb270f8e59c
hunghq21 <hunghq21@fpt.com>
2023-11-29 09:44:38 +0700

    km

--- App.tsx
+++ App.tsx
@@ -1,23 +1,25 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from './src/screens/HomeScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import DetailScreen from './src/screens/DetailScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MyTabs from './src/navigation';
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MyTabBar from './src/navigation';
+const Tab = createBottomTabNavigator();
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-    <MyTabs/>
-      {/* <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Welcome" component={WelcomeScreen}/>
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="Detail" component={DetailScreen}/>
-      </Stack.Navigator> */}
+      <Tab.Navigator
+        screenOptions={{headerShown: false}}
+        tabBar={props => <MyTabBar {...props} />}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Detail" component={DetailScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
